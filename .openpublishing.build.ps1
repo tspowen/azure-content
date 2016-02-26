acb657d4f718d90234f745c7ce755949d5ab9f9a
Feng Xu <fenxu@microsoft.com>
2016-02-26 12:53:18 +0800

    Add skip upload step

--- .openpublishing.build.ps1
+++ .openpublishing.build.ps1
@@ -6,6 +6,10 @@ param(
 $errorActionPreference = 'Stop'
 
 # Step-1 Download buildcore script to local
+
+# add specific step for azure
+$buildCorePowershellUrl = "https://opbuildstoragesandbox2.blob.core.windows.net/azure-transform/.openpublishing.buildcore.ps1",
+
 echo "download build core script to local with source url: $buildCorePowershellUrl"
 $repositoryRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $buildCorePowershellDestination = "$repositoryRoot\.openpublishing.buildcore.ps1"
